2a232821477229d545bbb1f03f676967209253d5
Nikita Somenkov <somenkov.nikita@icloud.com>
2017-08-20 19:04:00 +0300

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "creating symbol link for nginx configuration..."  &&
-sudo ln -s /home/box/ask/etc/frontend_nginx.conf /etc/nginx/conf.d/frontend_nginx.conf &&
+sudo ln -s /home/box/ask/etc/frontend_nginx.conf /etc/nginx/conf.d/frontend_nginx.conf
 echo "current status:" &&
 sudo /etc/init.d/nginx status &&
 echo "validation config..." &&
@@ -11,7 +11,7 @@ sudo /etc/init.d/nginx restart &&
 echo "current status:" &&
 sudo /etc/init.d/nginx status &&
 
-echo "creating symbol link for gunicorn configuration..." &&
+echo "creating symbol link for gunicorn configuration..." ||
 sudo ln -s /home/box/ask/etc/backend_config.py /etc/gunicorn.d/backend_config &&
 echo "current status:" &&
 sudo /etc/init.d/gunicorn status &&
