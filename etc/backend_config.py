126d82adb5cc1f5f90a89fc17b31f6ccf4bba1eb
Nikita Somenkov <somenkov.nikita@icloud.com>
2017-08-20 19:18:13 +0300

    Fix backend_config

--- etc/backend_config.py
+++ etc/backend_config.py
@@ -1,1 +1,11 @@
-bind = "0.0.0.0:8000"
+CONFIG = {
+    'mode': 'django',                                                           
+    'environment': {                                                            
+        'PYTHONPATH': '/home/box/web/ask',
+    },
+    'args': (                                                                   
+        '--bind=0.0.0.0:8000',
+        '--workers=4',                                                          
+        '--worker-class=ask.wsgi:application#sync',                                
+    ),                                                                          
+}      
