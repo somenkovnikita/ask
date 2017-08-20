842ac3ae172e3786a3f955c9684f52d372b5847e
Nikita Somenkov <somenkov.nikita@icloud.com>
2017-08-20 20:59:27 +0300

    Fix backend config

--- etc/backend_config.py
+++ etc/backend_config.py
@@ -1,11 +1,9 @@
 CONFIG = {
-    'mode': 'django',                                                           
-    'environment': {                                                            
-        'PYTHONPATH': '/home/box/web/ask',
-    },
+    'mode': 'wsgi',
+    'working_dir': '/home/box/web/ask',
     'args': (                                                                   
         '--bind=0.0.0.0:8000',
         '--workers=4',                                                          
-        '--worker-class=ask.wsgi:application#sync',                                
-    ),                                                                          
+        'ask.wsgi',
+    )
 }      
